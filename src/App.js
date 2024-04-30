2f5ba448b557acab0b2d144e82f0c02dfb95a85b
OchiMoto21 <nikencarolina@gmail.com>
2024-04-30 19:38:22 +0700

    Refactor Display Matrix WIP

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import './App.css';
+import "./App.css";
 
-import {Component} from 'react';
-import DynamicProgramming from './components/DynamicProgramming';
+import { Component } from "react";
+import DynamicProgramming from "./components/DynamicProgramming";
 class App extends Component {
-
-  
-  
   render() {
-
     return (
-    <div className="App">
-      <main style={{marginTop:"3rem"}}>
-
-      <h1>Visualization of Longest Palindrome Substring</h1>
-      </main>
-      <DynamicProgramming />
-      {/* "Testing" */}
-    </div>
-    )
-  };
+      <div className="App">
+        <main style={{ marginTop: "3rem" }}>
+          <h1>Visualization of Longest Palindrome Substring</h1>
+        </main>
+        <DynamicProgramming />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/DisplayMatrix.js
+++ src/components/DisplayMatrix.js
@@ -1,62 +1,105 @@
-import './DisplayMatrix.css'
-
+import "./DisplayMatrix.css";
 
 function DPTable({ dp, string, cellChanged }) {
-    // if (input.length === 0 && dp === undefined) return; 
-    const input = string.split("")
+  // if (input.length === 0 && dp === undefined) return;
+  const input = string.split("");
 
-    const [rowChanged, columnChanged] = cellChanged
+  const [rowChanged, columnChanged] = cellChanged;
 
-    const index_array = [];
-    index_array.push(<td key={"Column Header " + -2} className='table-header'></td>)
-    index_array.push(<td key={"Column Header " + -1} className='table-header'><code>j</code></td>)
+  const index_array = GenerateTableHeader(string, columnChanged);
+  return (
+    <div className="table-wrapper">
+      <table>
+        <tbody>
+          <tr>{index_array}</tr>
 
-    for (let i = 0; i < input.length; i++) {
-        index_array.push(<td key={"Column Header " + i} className={'table-header' + (i === columnChanged ? (" column-changed") : (""))}>
-                <p>
-                    {i}
-                </p>
-            </td>)
-    }
+          <tr>
+            <td className="table-header">
+              <code>i</code>
+            </td>
+            <td className="table-header"></td>
+            {input.map((cell, cellIndex) => (
+              <td
+                key={cell + cellIndex}
+                className={
+                  "table-header" +
+                  (cellIndex === columnChanged ? " column-changed" : "")
+                }
+              >
+                <p>{cell}</p>
+              </td>
+            ))}
+          </tr>
+          {dp.map((row, rowIndex) => (
+            <tr
+              key={rowIndex}
+              className={rowIndex === rowChanged ? "row-changed" : ""}
+            >
+              <td
+                className={
+                  "table-header" +
+                  (rowIndex === rowChanged ? " row-changed" : "")
+                }
+              >
+                {rowIndex}
+              </td>
+              <td
+                className={
+                  "table-header" +
+                  (rowIndex === rowChanged ? " row-changed" : "")
+                }
+              >
+                {input[rowIndex]}
+              </td>
 
-
-    return (
-        <div className='table-wrapper'>
-            <table>
-                <tbody>
-                    <tr>
-                        {index_array}
-                    </tr>
-
-                    <tr>
-                        <td className='table-header'><code>i</code></td>
-                        <td className='table-header'></td>
-                        {input.map((cell, cellIndex) => (
-                            <td key={cell + cellIndex} className={'table-header' + (cellIndex === columnChanged ? (" column-changed") : (""))}>
-                                <p>
-                                    {cell}
-                                </p>
-                            </td>
-                        ))}
-                    </tr>
-                    {dp.map((row, rowIndex) => (
-                        <tr key={rowIndex} className={rowIndex === rowChanged ? ("row-changed") : ("")}>
-                            <td className={'table-header' + (rowIndex === rowChanged ? (" row-changed") : (""))}>{rowIndex}</td>
-                            <td className={'table-header' + (rowIndex === rowChanged ? (" row-changed") : (""))} >{input[rowIndex]}</td>
-
-                            {row.map((cell, cellIndex) => {
-                                return (<td className={
-                                    (cell ? "value-true" : "value-false") +
-                                    (rowIndex === rowChanged && cellIndex === columnChanged ? " value-changed" : "") +
-                                    (cellIndex === columnChanged ? " column-changed" : "")
-                                } key={cellIndex}>{cell ? '1' : '0'}</td>)
-                            })}
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
+              {row.map((cell, cellIndex) => {
+                return (
+                  <td
+                    className={
+                      (cell ? "value-true" : "value-false") +
+                      (rowIndex === rowChanged && cellIndex === columnChanged
+                        ? " value-changed"
+                        : "") +
+                      (cellIndex === columnChanged ? " column-changed" : "")
+                    }
+                    key={cellIndex}
+                  >
+                    {cell ? "1" : "0"}
+                  </td>
+                );
+              })}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
-// 
+function GenerateTableHeader({ string, columnChanged }) {
+  const input = string.split("");
+  const index_array = [];
+  index_array.push(
+    <td key={"Column Header " + -2} className="table-header"></td>
+  );
+  index_array.push(
+    <td key={"Column Header " + -1} className="table-header">
+      <code>j</code>
+    </td>
+  );
+
+  for (let i = 0; i < input.length; i++) {
+    index_array.push(
+      <td
+        key={"Column Header " + i}
+        className={
+          "table-header" + (i === columnChanged ? " column-changed" : "")
+        }
+      >
+        <p>{i}</p>
+      </td>
+    );
+  }
+  return index_array;
+}
+//
 export default DPTable;
